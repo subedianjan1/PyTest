f55ad268b8f6514121d1b17b1a534ac93999f937
Anj <subedianjan@yahoo.com.au>
2019-06-17 11:49:21 +1000

    fixture
    automate executing each time within in function

--- Test_Pyfixture.py
+++ Test_Pyfixture.py
@@ -5,41 +5,49 @@ import pytest
 to connect and disconnect hardware or intialize a object or database..
 This feature(Setup) will help to do it only once before the test. While teardown
 will help to release the setup at the end of the test.
+
+If somethig has to be run each time before each test function can use fixture.
+No need to type code each time for function.
 )
 '''
 
-setupState =  None  #Global Variable
-
-def setup_module(module):
-  #  global setupState
+@pytest.fixture
+def setupState():
+    setupState  = 0
     #code that Intitialises the setup or Hware
-    print(" Initialed the database or HWare")
-    print( "Connected to Hardware")
-  
-def teardown_module(module):
-    print(" Disconnect Hardware or close the databae")
-   
+    setupState  = 1
+    return setupState
+     
 @pytest.mark.number #option to only run seleted test marked by number
-def test_add():
+def test_add(setupState):
+    if setupState == 1:
+        print("Initialed the database or HWare")
     assert MathFunc.add(7,3) == 10
     assert MathFunc.add(7) == 9    # second is given default in function as 2
     
     
 @pytest.mark.number
-def test_product():
-    assert MathFunc.product(7,3) == 21
-    assert MathFunc.product(7) == 14
+def test_product(setupState):
+     if setupState == 1:
+        print("Initialed the database or HWare")
+     assert MathFunc.product(7,3) == 21
+     assert MathFunc.product(7) == 14
     
  #instead of == we can also use <,>,=>. != etc
 
 #@pytest.mark.skip(reason = "do not run the test")
 #Skipif can also be used if conditonal skip is required
-def test_Junk():
+def test_Junk(setupState):
+    if setupState == 1:
+        print("Initialed the database or HWare")
+    assert MathFunc.product(7,3) == 21
     print(MathFunc.product (7,3),'Testing of print')# use -s to capture print
-    #assert 'ii' == MathFunc.add ( 'i')
+    
      
 @pytest.mark.string #option to only run seleted test marked by number
-def test_add_Strings():
+def test_add_Strings(setupState):
+    if setupState == 1:
+        print("Initialed the database or HWare")
     result = MathFunc.add('Hello', 'World')
     assert result == 'HelloWorld'
     assert type(result) is str
@@ -47,7 +55,9 @@ def test_add_Strings():
     assert 'Helllo'not in result
       
 @pytest.mark.string 
-def test_product_Strings():
+def test_product_Strings(setupState):
+    if setupState == 1:
+        print("Initialed the database or HWare")
     assert MathFunc.product( 'Hello ', 3 ) == 'Hello Hello Hello '
     result  = MathFunc.product('Hello')
     assert result == 'HelloHello'
