66266fe92eaa9cccf51cf04033bde08a88f2d23c
Anj <subedianjan@yahoo.com.au>
2021-10-25 17:46:44 +1100

    fixture_scope Module

--- Test_Pyfixture_moduleN.py
+++ Test_Pyfixture_moduleN.py
@@ -8,13 +8,18 @@ Once the fixture is defined. it can be passed as a paramerter to to the test fun
 After which the fixture function can be called. It is very useful feature as
 repetion of same code in multiple places can be avoided.
 
+By making the scope Modular, the fixture
+automatically called by every test function and no need to pass an argument
+
 
 cd C:\Projects\Github\Python-Test-Work\PyTest
 pytest -v -s Test_Pyfixture_mpdule.py
 
 '''
 
-@pytest.fixture(scope='module', autouse = True)
+setupState = 'None'
+
+@pytest.fixture(scope='module')
 def setupState(): #Afer scoping for module. Only supposed to be executed onec.
     print ("******************Setup*****************")
     global setupState
@@ -33,20 +38,16 @@ def test_product():
      assert MathFunc.product(7,3) == 21
      assert MathFunc.product(7) == 14
      print (setupState) 
-   
- #instead of == we can also use <,>,=>. != etc
-
+#instead of == we can also use <,>,=>. != etc
 #@pytest.mark.skip(reason = "do not run the test")
 #Skipif can also be used if conditonal skip is required
 def test_Junk():
-
     assert MathFunc.product(7,3) == 21
     print(MathFunc.product (7,3),'Testing of print')# use -s to capture print
     print (setupState) 
      
 @pytest.mark.string #option to only run seleted test marked by number
 def test_add_Strings():
-
     result = MathFunc.add('Hello', 'World')
     assert result == 'HelloWorld'
     assert type(result) is str
@@ -56,7 +57,6 @@ def test_add_Strings():
       
 @pytest.mark.string 
 def test_product_Strings():
-
     assert MathFunc.product( 'Hello ', 3 ) == 'Hello Hello Hello '
     result  = MathFunc.product('Hello')
     assert result == 'HelloHello'
